e7724737ac975a770b373018a9dc10e759a715b1
Deevyne99 <kaludivine545@gmail.com>
2024-04-29 09:49:40 +0100

    Build: Built create post screen and feed screen

---
+++ src/screens/FeedScreen.js
@@ -0,0 +1,13 @@
+import FeedPost from '../components/FeedPost'
+import posts from '../../assets/data/posts.json'
+import { FlatList } from 'react-native'
+const FeedScreen = () => {
+  return (
+    <FlatList
+      data={posts}
+      renderItem={({ item }) => <FeedPost post={item} />}
+    />
+  )
+}
+
+export default FeedScreen
